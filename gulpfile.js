d3308e4df11ff63ca81f3732a325258244fa27e2
Phillip Creglow <phillipcreglow@msn.com>
2017-07-05 19:08:08 -0400

    Added scriptsConcat task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,20 @@
 "use strict";
 
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+
+gulp.task("scriptsConcat", function() {
+  return gulp.src([
+    'js/jquery.js',
+    'js/fastclick.js',
+    'js/foundation.js',
+    'js/foundation.equalizer.js',
+    'js/foundation.reveal.js'
+  ])
+  .pipe(concat('scripts.js'))
+  .pipe(gulp.dest('js'));
+});
+
+gulp.task("default", function() {
+  gulp.start('build');
+});
