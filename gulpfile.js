985e970687adfcbb5931d5d1c1e0112ef8325c30
Phillip Creglow <phillipcreglow@msn.com>
2017-07-05 19:12:16 -0400

    Set gulp-uglify as a dev dependency and add uglify task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task("scriptsConcat", function() {
   return gulp.src([
@@ -9,9 +10,16 @@ gulp.task("scriptsConcat", function() {
     'js/fastclick.js',
     'js/foundation.js',
     'js/foundation.equalizer.js',
-    'js/foundation.reveal.js'
+    'js/foundation.reveal.js',
+    'js/scripts.js'
   ])
-  .pipe(concat('scripts.js'))
+  .pipe(concat('app.js'))
+  .pipe(gulp.dest('js'));
+});
+
+gulp.task("scriptsMinify", ["scriptsConcat"], function() {
+  return gulp.src("js/app.js")
+  .pipe(uglify())
   .pipe(gulp.dest('js'));
 });
 
