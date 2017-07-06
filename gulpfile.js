5bffdb0c8f6fb8051cf2d316b567a6f2fa1bda0c
Phillip Creglow <phillipcreglow@msn.com>
2017-07-05 22:46:43 -0400

    Refactor gulpfile to remove sourcemaps and rename, refactor css and js tasks in gulpfile, refactor files paths for css and js in index.html

--- gulpfile.js
+++ gulpfile.js
@@ -3,62 +3,50 @@
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    maps = require('gulp-sourcemaps'),
     del = require('del'),
     cleanCSS = require('gulp-clean-css');
 
-gulp.task("scriptsConcat", function() {
-  return gulp.src([
-    'js/jquery.js',
-    'js/fastclick.js',
-    'js/foundation.js',
-    'js/foundation.equalizer.js',
-    'js/foundation.reveal.js'
-  ])
-  .pipe(maps.init())
-  .pipe(concat('app.js'))
-  .pipe(maps.write('./'))
-  .pipe(gulp.dest('js'));
-});
+// gulp.task("scriptsConcat", function() {
+//   return gulp.src([
+//     'js/jquery.js',
+//     'js/fastclick.js',
+//     'js/foundation.js',
+//     'js/foundation.equalizer.js',
+//     'js/foundation.reveal.js'
+//   ])
+//   .pipe(maps.init())
+//   .pipe(concat('app.js'))
+//   .pipe(maps.write('./'))
+//   .pipe(gulp.dest('js'));
+// });
+//
+// gulp.task("scriptsMinify", ["scriptsConcat"], function() {
+//   return gulp.src('js/app.js')
+//   .pipe(uglify())
+//   .pipe(concat('js/app.min.js'))
+//   .pipe(gulp.dest('js'));
+// });
 
-gulp.task("scriptsMinify", ["scriptsConcat"], function() {
-  return gulp.src("js/app.js")
+gulp.task('js', function() {
+  return gulp.src('js/*.js')
   .pipe(uglify())
-  .pipe(rename('js/app.min.js'))
+  .pipe(concat('app.min.js'))
   .pipe(gulp.dest('js'));
 });
 
-gulp.task("stylesConcat", function() {
-  return gulp.src([
-    'css/normalize.css',
-    'css/foundation.min.css',
-    'css/basics.css',
-    'css/menu.css',
-    'css/hero.css',
-    'css/photo-grid.css',
-    'css/modals.css',
-    'css/footer.css'
-  ])
-  .pipe(maps.init())
-  .pipe(concat('styles.css'))
-  .pipe(maps.write('./'))
-  .pipe(gulp.dest('css'));
-});
-
-gulp.task("stylesMinify", ["stylesConcat"], function() {
-  return gulp.src("css/styles.css")
+gulp.task('css', function() {
+  return gulp.src('css/*.css')
   .pipe(cleanCSS())
-  .pipe(rename('css/styles.min.css'))
+  .pipe(concat('styles.min.css'))
   .pipe(gulp.dest('css'));
 });
 
 gulp.task('cleanFiles', function() {
-  del(['dist', 'css/styles.css*', 'js/app.js*', 'img/social/*.png']);
+  del(['dist', 'css/styles.min.css*', 'js/app.js*', 'img/social/*.png']);
 });
 
-gulp.task("build", ['scriptsConcat', 'stylesConcat'], function() {
-  return gulp.src(["css/styles.css", "js/app.js", 'index.html',
+gulp.task("build", ['js', 'css'], function() {
+  return gulp.src(["css/styles.min.css", "js/app.min.js", 'index.html',
                    "img/**"], { base: './'})
             .pipe(gulp.dest('dist'));
 });
